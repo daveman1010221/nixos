91e6e8e7019023284c98962613b49b73ba08215c
David Shepard <daveman1010220@gmail.com>
2025-05-02 15:54:12 -0400

    moving around lols

--- shell/fish/functions/templated/lol.nix
+++ shell/fish/functions/templated/lol.nix
@@ -1,11 +1,14 @@
-function lol --description="lolcat inside cowsay"
+{ cowsayPath, ... }:
+
+''
+function lol --description="lolcat (dotacat) inside cowsay"
     echo $argv | \
-        cowsay -n -f (set cows (ls /usr/share/cowsay/cows); \
+        cowsay -n -f (set cows (ls ${cowsayPath}/share/cowsay/cows); \
         set total_cows (count $cows); \
         set random_cow (random 1 $total_cows); \
         set my_cow $cows[$random_cow]; \
         echo -n $my_cow | \
         cut -d '.' -f 1) -W 79 | \
-        lolcat --force | \
-        cat
+        dotacat
 end
+''
