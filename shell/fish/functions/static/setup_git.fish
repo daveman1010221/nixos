556ea4e5cb5fa1e43980a7fa4189d75ab0418f41
David Shepard <daveman1010220@gmail.com>
2025-06-04 09:11:20 -0400

    add a shell function for configuring git

---
+++ shell/fish/functions/static/setup_git.fish
@@ -0,0 +1,24 @@
+function setup_git
+    echo -n "Enter your Git user name: "
+    read git_user
+    echo -n "Enter your Git email: "
+    read git_email
+    echo -n "Do you want to configure Git credentials helper? (y/N): "
+    read use_creds
+
+    git config --global user.name "$git_user"
+    git config --global user.email "$git_email"
+
+    if test (string match -r '^[Yy]' -- $use_creds)
+        echo -n "Enter your Git username (for HTTPS auth): "
+        read gh_user
+        echo -n "Enter your Git password or PAT: "
+        read -l gh_pass
+
+        git config --global credential.helper store
+        echo "https://$gh_user:$gh_pass@github.com" > ~/.git-credentials
+        chmod 600 ~/.git-credentials.d/github
+    end
+
+    echo "Git is configured. Welcome to 2009."
+end
