2547db447fcc9256428a20321b47b686321dce48
David Shepard <daveman1010220@gmail.com>
2025-06-04 09:46:17 -0400

    fix setup_git

--- shell/fish/functions/static/setup_git.fish
+++ shell/fish/functions/static/setup_git.fish
@@ -1,16 +1,14 @@
 function setup_git
-    read --prompt "Enter your Git user name: " git_user
-    read --prompt "Enter your Git email: " git_email
-    read --prompt "Do you want to configure Git credentials helper? (y/N): " use_creds
+    read --prompt-str "Enter your Git user name: " git_user
+    read --prompt-str "Enter your Git email: " git_email
+    read --prompt-str "Do you want to configure Git credentials helper? (y/N): " use_creds
 
     git config --global user.name "$git_user"
     git config --global user.email "$git_email"
 
     if test (string match -r '^[Yy]' -- $use_creds)
-        echo -n "Enter your Git username (for HTTPS auth): "
-        read gh_user
-        echo -n "Enter your Git password or PAT: "
-        read -l gh_pass
+        read --prompt-str "Enter your Git username (for HTTPS auth): " gh_user
+        read --prompt-str "Enter your Git password or PAT: " -l gh_pass
 
         git config --global credential.helper store
 
