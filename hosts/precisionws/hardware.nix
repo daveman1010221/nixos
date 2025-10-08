39ac4adfaebe2622621d8d39b20137b1bc51d956
David Shepard <daveman1010220@gmail.com>
2025-10-08 11:08:28 -0400

    feat(hardware): enable 32-bit graphics and add extra GPU libs
    Expands graphics configuration to enable 32-bit support and include key video acceleration libraries (libva, libvdpau, libglvnd, mesa).
    
    Improves compatibility for GPU-accelerated apps, browsers, and container workloads requiring legacy 32-bit graphics interfaces.

--- hosts/precisionws/hardware.nix
+++ hosts/precisionws/hardware.nix
@@ -6,7 +6,16 @@
     enableAllHardware = true;
     cpu.intel.updateMicrocode = true;
     cpu.x86.msr.enable = true;
-    graphics.enable = true;
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgsForHost; [
+        libva
+        libvdpau
+        libglvnd
+        mesa
+      ];
+    };
   
     # Effectively, this option adds udev rules that allow a
     # non-privileged user to modify keyboard firmware.
