07b56dd33d9dd69f39bb4b11d929d20a3944246a
David Shepard <daveman1010220@gmail.com>
2026-03-27 10:14:33 -0400

    chore(flake): fix warnings for nixos_update

--- shell/fish/functions/static/nixos_update.fish
+++ shell/fish/functions/static/nixos_update.fish
@@ -2,7 +2,6 @@ function nixos_update --description 'Update NixOS configuration with automatic b
     # Check if boot is mounted
     boot_is_mounted "quiet"
     set -l boot_was_mounted $status
-
     # If boot was not mounted originally, mount it now
     if test $boot_was_mounted -ne 0
         echo "Boot volumes are not fully mounted. Mounting them now..."
@@ -14,30 +13,29 @@ function nixos_update --description 'Update NixOS configuration with automatic b
     else
         echo "Boot volumes are already mounted."
     end
-
     # Run nixos-rebuild switch
-    echo "Running nixos-rebuild switch..."
+    echo "Running nixos-rebuild switch, please wait..."
     pushd /etc/nixos
-    sudo nixos-rebuild switch \
-    	--flake .#precisionws \
-        --override-input secrets-empty path:/boot/secrets/flakey.json \
-        --option extra-sandbox-paths /boot/secrets \
-        --option allowed-impure-host-deps /boot/secrets
+    begin
+        sudo nixos-rebuild switch \
+            --flake .#precisionws \
+            --no-write-lock-file \
+            --override-input secrets-empty path:/boot/secrets/flakey.json \
+            --option extra-sandbox-paths /boot/secrets \
+            --option allowed-impure-host-deps /boot/secrets
+    end 2>&1 | grep -Ev 'not writing modified lock file|Updated input .secrets-empty|^\s+'\''path:\./secrets-empty|^\s+[→]\s+'\''path:/boot/secrets/flakey'
     popd
     set rebuild_status $status
-
     # After the rebuild, unmount only if we mounted them in this function
     if test $boot_was_mounted -ne 0
         echo "Unmounting boot volumes (since they were not mounted originally)."
         boot_toggle_mounts
         if test $status -ne 0
             echo "Failed to unmount boot volumes."
-            # Optionally handle the error here
         end
     else
         echo "WARNING: Boot volumes remain mounted."
     end
-
     # Return nixos-rebuild's status
     return $rebuild_status
 end
